a9377e9f0512a52899320503b23f2f5a95aa6e3a
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2026-08-21 22:36:35 -0300

    Red Reign effect: Meat drop reduced in 50%

--- neoforge-project/RedReign/src/main/java/io/github/anttluca/red_reign/events/runtime/RRWorldEffetcsWorks.java
+++ neoforge-project/RedReign/src/main/java/io/github/anttluca/red_reign/events/runtime/RRWorldEffetcsWorks.java
@@ -3,10 +3,14 @@ package io.github.anttluca.red_reign.events.runtime;
 import io.github.anttluca.red_reign.RedReign;
 import io.github.anttluca.red_reign.world.data.RedReignWorldData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.gamerules.GameRules;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
 import net.neoforged.neoforge.event.entity.living.LivingHealEvent;
 import net.neoforged.neoforge.event.level.LevelEvent;
 
@@ -25,6 +29,22 @@ public class RRWorldEffetcsWorks {
     public static void onLivingHeal(LivingHealEvent event) {
         // All entities heal reducerd in 60%
         event.setAmount(event.getAmount() * 0.4F);
+    }
+
+    @SubscribeEvent
+    public static void onAnimalDrops(LivingDropsEvent event) {
+        // Every meat drop has a 50% chance of not happening
+        if (event.getEntity() instanceof Animal animal
+            && animal.level()instanceof ServerLevel serverLevel) {
+                event.getDrops().removeIf(drop -> {
+                    ItemStack stack = drop.getItem();
+                    if (stack.is(ItemTags.MEAT)) {
+                        return serverLevel.getRandom().nextFloat() < 0.5F;
+                    }
+
+                    return false;
+                });
+        }
     }
 
     @EventBusSubscriber(modid = RedReign.MODID)
