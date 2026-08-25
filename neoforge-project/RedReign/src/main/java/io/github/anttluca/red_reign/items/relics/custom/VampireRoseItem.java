61af5c2ec874dba5b33c16d10cca1c136ece826d
Luca Anthony <123425103+antth-Luca@users.noreply.github.com>
2026-08-25 18:47:31 -0300

    Life Steal attribute and Vampire Rose item

--- neoforge-project/RedReign/src/main/java/io/github/anttluca/red_reign/items/relics/custom/VampireRoseItem.java
+++ neoforge-project/RedReign/src/main/java/io/github/anttluca/red_reign/items/relics/custom/VampireRoseItem.java
@@ -2,8 +2,11 @@ package io.github.anttluca.red_reign.items.relics.custom;
 
 import io.github.anttluca.red_reign.handlers.CurioItemsHandler;
 import io.github.anttluca.red_reign.init.InitItems;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.puffish.attributesmod.api.PuffishAttributes;
+import top.theillusivec4.curios.api.CurioAttributeModifiers;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
@@ -20,4 +23,17 @@ public class VampireRoseItem extends Item implements ICurioItem {
                 && !CurioItemsHandler.hasCurio(context.entity(), InitItems.VAMPIRE_ROSE.get());
     }
 
+    @Override
+    public CurioAttributeModifiers getDefaultCurioAttributeModifiers(ItemStack stack) {
+        return CurioAttributeModifiers.builder()
+                .addModifier(
+                        PuffishAttributes.LIFE_STEAL,
+                        new AttributeModifier(
+                                InitItems.VAMPIRE_ROSE.getId(),
+                                0.05F,
+                                AttributeModifier.Operation.ADD_MULTIPLIED_BASE
+                        ),
+                        "charm"
+                ).build();
+    }
 }
