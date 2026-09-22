e492acb4bf7085bc60838982ce546d89be4793f6
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2026-09-22 20:53:01 -0300

    Update RRClientPayloadHandler.java
    Add sounds!

--- neoforge-project/RedReign/src/main/java/io/github/anttluca/red_reign/handlers/RRClientPayloadHandler.java
+++ neoforge-project/RedReign/src/main/java/io/github/anttluca/red_reign/handlers/RRClientPayloadHandler.java
@@ -2,7 +2,9 @@ package io.github.anttluca.red_reign.handlers;
 
 import io.github.anttluca.red_reign.networking.packets.RRDisplayItemActivationPayload;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
@@ -19,10 +21,34 @@ public class RRClientPayloadHandler {
                     living.getX(),
                     living.getY(),
                     living.getZ(),
-                    SoundEvents.TOTEM_USE,
+                    SoundEvents.ENDER_DRAGON_GROWL,
                     living.getSoundSource(),
                     1.0F, 1.0F,
                     false
+                );
+                mc.getSoundManager().playDelayed(
+                        new SimpleSoundInstance(
+                                SoundEvents.GHAST_AMBIENT,
+                                living.getSoundSource(),
+                                1.0F, 1.0F,
+                                RandomSource.create(entity.getRandom().nextLong()),
+                                living.getX(),
+                                living.getY(),
+                                living.getZ()
+                        ),
+                        20
+                );
+                mc.getSoundManager().playDelayed(
+                    new SimpleSoundInstance(
+                        SoundEvents.GHAST_AMBIENT,
+                        living.getSoundSource(),
+                        1.0F, 1.0F,
+                        RandomSource.create(entity.getRandom().nextLong()),
+                        living.getX(),
+                        living.getY(),
+                        living.getZ()
+                    ),
+                    30
                 );
             }
         });
